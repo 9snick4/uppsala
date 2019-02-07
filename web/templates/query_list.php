6d57242fcf7a591974ed2fa436a4a13619e09faa
9snick4 <nic17014@byui.edu>
2019-02-07 16:46:26 +0100

    fixing queries on query_list.php

--- web/templates/query_list.php
+++ web/templates/query_list.php
@@ -22,7 +22,7 @@ try
     $stmt = $db->prepare('SELECT mapname FROM map ORDER BY mapname');
     break;
   case 'c':
-    $stmt = $db->prepare('SELECT cityname AS Name,Latitute,Longitude,Elevation,citypopulation AS Population,mapname AS Map 
+    $stmt = $db->prepare('SELECT cityname AS City,Latitude,Longitude,Elevation,citypopulation AS Population,mapname AS Map 
                           FROM city INNER JOIN mapcity ON city.cityid = mapcity.cityid INNER JOIN map ON mapcity.mapid = map.mapid
                           ORDER BY cityname, mapname');
     break;
@@ -30,6 +30,7 @@ try
     $stmt = $db->prepare('SELECT gamername AS Name,gamedate AS Date,gametime AS Time,points,mapname AS Map 
                           FROM game INNER JOIN gamer ON game.gamerid = gamer.gamerid INNER JOIN map ON game.mapid = map.mapid
                           ORDER BY gamedate, gametime DESC');
+    break;
   case 'u':
   $stmt = $db->prepare('SELECT gamername FROM gamer ORDER BY gamername');
     break;
