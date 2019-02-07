130785f360536dbdafa457d73bbf5eeb7370db2a
9snick4 <nic17014@byui.edu>
2019-02-07 17:25:04 +0100

    test Object.keys

--- web/javascript/jqueryshow.js
+++ web/javascript/jqueryshow.js
@@ -2,7 +2,7 @@ function showAjaxList(t)
 {
     $.get( "templates/query_list.php", { table: t}).done(function(data) {
             var result = JSON.parse(data);
-            alert(Object.keys(result));   
+            alert(Object.keys(result[0]));   
         });
         
         
