a2515c1f7de469c0258046f83cad2d1a40147358
9snick4 <nic17014@byui.edu>
2019-02-07 16:30:57 +0100

    bootstrap on main_list.php

--- web/includes/main_list.php
+++ web/includes/main_list.php
@@ -1,12 +1,16 @@
 <div class="container">
 	<div class="row align-items-center">
-		<h2>Select the list to display</h2>
-		<select class="select" autofocus onchange="window.location.replace('templates/querylist.php?table=' + this.value);">
-			<option value="m">Maps</option>
-			<option value="c">Cities</option>
-			<option value="g">Games</option>
-			<option value="u">Gamers</option>
-		</select> 
-
+		<h2 class="col-12">Select the list to display</h2>
+		<div class="dropdown">
+			<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				Dropdown button
+			</button>
+			<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				<a class="dropdown-item" href="templates/query_list.php?table=m">Maps</a>
+				<a class="dropdown-item" href="templates/query_list.php?table=c">Cities</a>
+				<a class="dropdown-item" href="templates/query_list.php?table=g">Games</a>
+				<a class="dropdown-item" href="templates/query_list.php?table=u">Gamers</a>
+			</div>
+		</div>
 	</div>
 </div>
