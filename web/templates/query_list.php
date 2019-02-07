f50f3c545d4c08481fcc7797f102c374a9fd300c
9snick4 <nic17014@byui.edu>
2019-02-07 16:34:03 +0100

    bootstrap on main_list.php

--- web/templates/query_list.php
+++ web/templates/query_list.php
@@ -14,12 +14,7 @@ try
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+
 $stmt = "";
 switch ($table)
 {
@@ -42,4 +37,10 @@ switch ($table)
 //$stmt->bindValue(':id', $table, PDO::PARAM_INT);
 $stmt->execute();
 echo json_encode(fetchAll(PDO::FETCH_ASSOC));
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 ?>
