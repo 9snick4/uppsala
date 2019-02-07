12b38b8c21653e71bbc2287561f4d8975f69c463
9snick4 <nic17014@byui.edu>
2019-02-07 17:42:18 +0100

    dynamic table build

--- web/javascript/jqueryshow.js
+++ web/javascript/jqueryshow.js
@@ -2,7 +2,20 @@ function showAjaxList(t)
 {
     $.get( "templates/query_list.php", { table: t}).done(function(data) {
             var result = JSON.parse(data);
-            alert(Object.keys(result[0]));   
+            var columnNames = Object.keys(result[0]);   
+            var table = $('<table>').addClass('table').addClass('table-hover').addClass('table-responsive').append($('<thead>')).append($('<th>'));
+            for(i=0; i<columnNames.length; i++){
+                var col = $('<th>').attr("scope","col").text(columnNames[i]).css('textTransform', 'capitalize');
+                table.append(col);
+            }
+            table.append($('<tbody>'));
+            for(i=0; i<result.length; i++) {
+                for (j = 0; i<columnNames.length; j++) {
+                    var row = $('<tr>').text(result[0][columnNames[j]]);
+                    table.append(row);
+                }
+            }
+            $('#table-placeholder').append(table);
         });
         
         
