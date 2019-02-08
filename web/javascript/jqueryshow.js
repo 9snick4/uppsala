15bf6905ce67825e2e2c0030d7fcacf7bdb8b21b
9snick4 <nic17014@byui.edu>
2019-02-08 10:09:07 +0100

    fixed table structure

--- web/javascript/jqueryshow.js
+++ web/javascript/jqueryshow.js
@@ -3,19 +3,23 @@ function showAjaxList(t)
     $.get( "templates/query_list.php", { table: t}).done(function(data) {
             var result = JSON.parse(data);
             var columnNames = Object.keys(result[0]);   
-            var table = $('<table>').addClass('table').addClass('table-hover').addClass('table-responsive').append($('<thead>')).append($('<th>'));
+            var table = $('<table>').addClass('table').addClass('table-hover').addClass('table-responsive'); //.append($('<thead>')).append($('<th>'));
+            var thead = $('<thead>').append($('<th>'));
             for(i=0; i<columnNames.length; i++){
                 var col = $('<th>').attr("scope","col").text(columnNames[i]).css('textTransform', 'capitalize');
-                table.append(col);
+                thead.append(col);
             }
-            table.append($('<tbody>'));
+            table.append(thead);
+            var tbody = $('<tbody>') 
             for(i=0; i<result.length; i++) {
-                table.append($('<tr>')).append($('<td>').text(i));
+                var tr =$('<tr>').append($('<td>').text(i));
                 for (j = 0; j<columnNames.length; j++) {
                     var row = $('<td>').text(result[0][columnNames[j]]);
-                    table.append(row);
+                    tr.append(row);
                 }
+                tbody.append(tr);
             }
+            table.append(tbody);
             $('#table-placeholder').html(table);
         });
         
