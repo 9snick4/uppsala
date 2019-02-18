818f6887aa50cbdb8314979dde2a1c36c956dfae
9snick4 <nic17014@byui.edu>
2019-02-18 10:51:33 +0100

    newgame page

---
+++ web/includes/main_newgame.php
@@ -0,0 +1,34 @@
+<div class="container">
+	<div class="justify-content-center">
+		<div class="row display-4">
+			New game
+		</div>
+		<form id="reg-form" class="needs-validation" method="GET" action="play.php">
+			<div class="form-group">
+				<label for="gamer-select">Who's playing?</label>
+				<select class="custom-select" size="3" id="gamer-select" name="gamer-select">
+					<?php
+						require("/templates/dbConnect.php");
+						$db = get_db();
+						foreach ($db->query("SELECT gamerid, gamername FROM name ORDER BY gamername") as $row)
+						{
+							echo '<option value='.$row["cityid"].'>'.$row["cityname"].'</option>';
+						}
+					?>
+				</select>
+				<label for="map-select">Which map do you want to play with?</label>
+				<select class="custom-select" size="3" id="map-select" name="map-select">
+					<?php
+						$db = get_db();
+						foreach ($db->query("SELECT mapid, mapName FROM map ORDER BY mapname") as $row)
+						{
+							echo '<option value='.$row["mapid"].'>'.$row["mapname"].'</option>';
+						}
+					?>
+				</select>
+				<input type="text" class="d-none" name="form-id" id="form-id" value="a">
+				<button class="btn btn-primary" type="submit">Let's play!</button>
+			</div>
+		</form>
+	</div>
+</div>
---
+++ web/newgame.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <?php include 'includes/head.php';?>
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <?php include 'includes/navigation.php';?>
+    
+    <!-- Header -->
+    <?php include 'includes/header.php';?>
+    
+    <!-- Main -->
+    <?php include 'includes/main_newgame.php';?>
+    
+    <!-- Main -->
+    <?php include 'includes/footer.php';?>
+
+  </body>
+
+</html>
