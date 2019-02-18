85e16be01bf3c505aa571e1b99daf96c9ea6104a
9snick4 <nic17014@byui.edu>
2019-02-18 12:36:44 +0100

    fetch cities

--- web/includes/main_play.php
+++ web/includes/main_play.php
@@ -1,12 +1,26 @@
 <?php
+	require("phpobjects/city.php")
+	$citiesDeck = array();
 	if(!isset($_SESSION["gameid"]))
 	{
 		require("templates/dbConnect.php");
 		$db = get_db();
-		$pdo=$db->prepare("SELECT nextval('game_gameid_seq')");
+		$pdo=$db->prepare("SELECT currval('game_gameid_seq')");
 		$pdo->execute();
 		$_SESSION["gameid"] = $pdo->fetch(PDO::FETCH_BOTH)[0];
-		echo $_SESSION["gameid"];
-	}
+		$_SESSION["gamerid"] = $_GET["gamer_select"];
+		$_SESSION["mapid"] = $_GET["map_select"];
+		foreach ($db->query("SELECT city.cityid, cityname,Latitude,Longitude,citypopulation,Extension,Elevation FROM city ORDER BY random() LIMIT 15") as $row)
+		{
+			array_push($citiesDeck, new City ( $row["cityid"], $row["cityname"], $row["Latitude"],$row["Longitude"],$row["citypopulation"],$row["Extension"], $row["Elevation"]));
+		}
+}
 
 ?>
+
+<script>
+
+var stringCitiesDeck = "<?php json_encode($citiesDeck); ?>";
+var citiesDeck = JSON.parse(stringCitiesDeck);
+
+</script>
---
+++ web/phpobjects/city.php
@@ -0,0 +1,52 @@
+<?php
+    class City {
+        protected $cityid;
+        protected $cityname;        
+        protected $latitude;
+        protected $longitude;
+        protected $citypopulation;      
+        protected $extension;
+        protected $elevation;
+
+        public function __construct ( $cityid, $cityname, $latitude,$longitude,$citypopulation,$extension,$elevation) {
+          $this->cityid = $cityid;
+          $this->cityname = $cityname;
+          $this->latitude = $latitude;
+          $this->longitude = $longitude;
+          $this->citypopulation = $citypopulation;
+          $this->extension = $extension;
+          $this->elevation = $elevation;
+        }
+
+    }
+
+    public function getCityid() { 
+        return $this->cityname;
+    }
+    
+    public function getCityname() { 
+        return $this->cityid;
+    }
+    
+    public function getLatitude() { 
+        return $this->latitude;
+    }
+    
+    public function getLongitude() { 
+        return $this->longitude;
+    }
+    
+    public function getCitypopulation() { 
+        return $this->citypopulation;
+    }
+    
+    public function getExtension() { 
+        return $this->extension;
+    }
+    
+    public function getElevation() { 
+        return $this->elevation;
+    }
+    
+    
+?>
