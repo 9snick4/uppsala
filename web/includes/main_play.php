ce2fa334f208dd008f1d40d30fdc2d496f6ef33f
9snick4 <nic17014@byui.edu>
2019-02-21 09:58:38 +0100

    currval doesn't work as expected

--- web/includes/main_play.php
+++ web/includes/main_play.php
@@ -5,7 +5,7 @@
 	{
 		require("templates/dbConnect.php");
 		$db = get_db();
-		$pdo=$db->prepare("SELECT currval('game_gameid_seq')");
+		$pdo=$db->prepare("SELECT nextval('game_gameid_seq')");
 		$pdo->execute();
 		$_SESSION["gameid"] = $pdo->fetch(PDO::FETCH_BOTH)[0];
 		$_SESSION["gamerid"] = $_GET["gamer_select"];
