a485cdc25358dadd9adf26a85583212291e1f88f
9snick4 <nic17014@byui.edu>
2019-02-21 12:37:05 +0100

    deb

--- web/includes/main_play.php
+++ web/includes/main_play.php
@@ -1,4 +1,5 @@
 <?php
+	require("phpobjects/city.php");
 	$citiesDeck = array();
 	if(!isset($_SESSION["gameid"]))
 	{
@@ -12,7 +13,10 @@
 		$db = get_db();
 		foreach ($db->query("SELECT city.cityid, cityname,latitude,longitude,citypopulation,Extension,Elevation FROM city ORDER BY random() LIMIT 15") as $row)
 		{
-			array_push($citiesDeck, "cityid"=>$row["cityid"], "cityname"=>$row["cityname"],"latitude"=>$row["latitude"],"longitude"=>$row["longitude"],"citypopulation"=>$row["citypopulation"],"Extension"=>$row["Extension"],"Elevation"=>$row["Elevation"],);		
+			$city = new City($row["cityid"], $row["cityname"], $row["latitude"],$row["longitude"],$row["citypopulation"],$row["Extension"], $row["Elevation"]);
+			array_push($citiesDeck, $city);
+			
+			
 		}
 		var_dump($citiesDeck);
 	}
